5c5524a01ba98b02c510f684d346ed619387a496
cajunken <kenlem@outlook.com>
2015-08-21 14:45:10 -0700

    added comments

--- getseqnums.sh
+++ getseqnums.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-# echo out vars for troubleshooting
-#echo 'getseqnums device is ' $1
-#echo 'user is ' $2
-#echo 'passwd is ' $3
+# Vars passed in are:  device ($1), username ($2), password ($3)
+# This script is called by blackholeip.py and gets the last sequence number and writes it to out.txt
 
 ./vty_runcmd.exp -h $1 -u $2 -p $3 -e enablepass, method = ssh < commands.txt > outfiles/out.txt
 
